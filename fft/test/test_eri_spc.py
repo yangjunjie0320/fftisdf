c1f0c4cd8ce5b4538bf03f8e009fa742d3432d96
Junjie Yang <yangjunjie0320@gmail.com>
2025-04-20 15:53:18 -0700

    update

--- fft/test/test_eri_spc.py
+++ fft/test/test_eri_spc.py
@@ -41,7 +41,7 @@ class EriSpcTest(unittest.TestCase):
     def tearDown(self):
         self.cell.stdout.close()
 
-    def test_fftisdf_eri_spc_mo(self):
+    def test_fftisdf_eri_spc_mo1(self):
         cell = self.cell
         kpts = self.kpts
         isdf_obj = self.isdf
@@ -58,6 +58,33 @@ class EriSpcTest(unittest.TestCase):
         coeff_spc = numpy.random.random((nkpts, nao, nmo))
         coeff_kpt = numpy.einsum("Rmp,Rk->Rmp", coeff_spc, phase)
         coeff_kpt = coeff_kpt.reshape(nkpts, nao, nmo)
+
+        eri_spc_ref = isdf_obj.ao2mo_kpt(coeff_kpt, kpts=kpts)
+        eri_spc_ref = numpy.einsum("KLMmnsl->mnsl", eri_spc_ref)
+        eri_spc_ref = eri_spc_ref.real
+
+        eri_spc_sol = isdf_obj.ao2mo_spc(coeff_kpt, kpts=kpts)
+        eri_spc_sol = eri_spc_sol.reshape(*eri_spc_ref.shape)
+
+        is_close = numpy.allclose(eri_spc_sol, eri_spc_ref, atol=1e-10)
+        self.assertTrue(is_close)
+
+    def test_fftisdf_eri_spc_mo4(self):
+        cell = self.cell
+        kpts = self.kpts
+        isdf_obj = self.isdf
+
+        nkpts = len(kpts)
+        nao = cell.nao_nr()
+        nmo = nao * 2
+
+        from fft.isdf_jk import get_phase, kpts_to_kmesh
+        wrap_around = isdf_obj.wrap_around
+        kpts, kmesh = kpts_to_kmesh(cell, kpts, wrap_around)
+        phase = get_phase(cell, kpts, kmesh, wrap_around)[1]
+
+        coeff_spc = [numpy.random.random((nkpts, nao, nmo)) for _ in range(4)]
+        coeff_kpt = [numpy.einsum("Rmp,Rk->Rmp", c, phase) for c in coeff_spc]
 
         eri_spc_ref = isdf_obj.ao2mo_kpt(coeff_kpt, kpts=kpts)
         eri_spc_ref = numpy.einsum("KLMmnsl->mnsl", eri_spc_ref)
